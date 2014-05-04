21c58fc84d8f36e4b5dbd5cecba75dba48354e23
Yazhong Liu <l900422@vip.qq.com>
2014-05-04 19:28:59 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,22 @@
+
+errno-extend
+================
+Error extension for v8
+
+### Installation
+```sh
+$ npm install errno-extent
+```
+
+### API
+
+* `ArgumentsError()`: An error that occurred by wrong arguments of a caller.
+
+* `AvoidAcessError()`: An error that user-land access the unaccessable callee.
+
+* `UnknowMethodError(name)`: Commonly used in HTTP handler.
+
+NB: TO BE COUTINUE... :)
+
+### License
+MIT
---
+++ index.js
@@ -0,0 +1,24 @@
+
+module.exports = {
+
+  /*
+   * arguments error
+   */
+  ArgumentsError: function() {
+    return new Error('wrong arguments');
+  },
+
+  /*
+   * avoid to access
+   */
+  AvoidAcessError: function() {
+    return new Error('avoid to access this member');
+  },
+
+  /*
+   * unknown method error
+   */
+  UnknowMethodError: function(val) {
+    return new Error('unknown method: '+val);
+  }
+};
